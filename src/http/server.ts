ab94b8a01b49c4ec937b3200ca46dff469231228
João Victor <joaovi.15.rodrigues@gmail.com>
2024-09-10 21:57:44 -0300

    add cors

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,8 +4,14 @@ import { createGoalRoute } from "./routes/create-goal";
 import { createGoalCompletionRoute } from "./routes/create-goal-completion";
 import { getPendingGoalRoute } from "./routes/get-pending-goals";
 import { getWeekSummaryRoute } from "./routes/get-week-summary";
+import fastifyCors from "@fastify/cors";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+app.register( fastifyCors, {
+    origin: "*"
+} )
+
+
 app.setValidatorCompiler( validatorCompiler );
 app.setSerializerCompiler( serializerCompiler );
 
